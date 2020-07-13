b060a8f6ca6606a56447557e50ac8c8bafdd5908
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-13 20:26:17 +0530

    [Mounika] Added:Assign the symbol to the player

--- TicTacTieGame.sh
+++ TicTacTieGame.sh
@@ -4,32 +4,32 @@ echo -e "Player vs Computer\n"
 row=3
 column=3
 numberOfShell=$(($row*$column))
-function resetBoard(){
-        board=( 1 2 3 4 5 6 7 8 9 )
-                 for (( columnValue=0,rowNumber=0;rowNumber<$row;columnValue=$(($columnValue+3)),rowNumber++ ))
-                 do
-                        echo -e "\t${board[columnValue]} | ${board[columnValue+1]} | ${board[columnValue+2]}"
-                          if (($rowNumber!=$(($row-1))))
-                          then
-                             echo -e "\t---------"
-                          fi
-                 done
-                    }
-resetBoard
+function displayBoard(){
+          board=( . . . . . . . . . )
+          for (( columnValue=0,rowNumber=0;rowNumber<$row;columnValue=$(($columnValue+3)),rowNumber++ ))
+          do
+                   echo -e "\t${board[columnValue]} | ${board[columnValue+1]} | ${board[columnValue+2]}"
+                      if (($rowNumber!=$(($row-1))))
+                      then
+                           echo -e "\t---------"
+                      fi
+         done
+                      }
+displayBoard
+
 
   function assignSymbol(){
-                                   assign=$((RANDOM%2))
-                                   if (($assign==1))
-                                   then
-                                           playerSymbol='X'
-                                           computerSymbol='O'
-                                   else
-                                           playerSymbol='O'
-                                           computerSymbol='X'
-                                   fi
+                       assign=$((RANDOM%2))
+                       if (($assign==1))
+                       then
+                             playerSymbol='X'
+                             computerSymbol='O'
+                       else
+                             playerSymbol='O'
+                             computerSymbol='X'
+                       fi
 
-                                  echo -e "\nPlayer 1 = $playerSymbol"
-                                  echo -e "Computer = $computerSymbol"
+                                  echo -e "\nPlayer 1 = $playerSymbol  Computer = $computerSymbol"
 
                                    }
   assignSymbol
