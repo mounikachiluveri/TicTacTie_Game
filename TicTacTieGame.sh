9e15b0dfa09225d7cdceb5388fc9fc4e1cc47326
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-14 10:19:24 +0530

    [Mounika] Refactor: Code refactored

--- TicTacTieGame.sh
+++ TicTacTieGame.sh
@@ -2,13 +2,15 @@
 echo -e "\n Welcome to Tic-Tac-Toe game.\n"
 echo -e "Player vs Computer\n"
 switchPlayer=0
+count=0
+TOTAL_CELL=9
  function resettingBoard() {
         board=(. . . . . . . . . .)
 }
 
  function displayBoard() {
 
-        echo -e "|  ${board[1]}  ||  ${board[2]}  ||  ${board[3]}  |\n---------------------\n| ${board[4]}   ||  ${board[5]}  ||  ${board[6]}  |\n---------------------\n|  ${board[7]}  ||  ${board[8]}  ||  ${board[9]}  |"
+        echo -e "****** Tic-Tac-Toe ******* \n|  ${board[1]}  ||  ${board[2]}  ||  ${board[3]}  |\n---------------------\n| ${board[4]}   ||  ${board[5]}  ||  ${board[6]}  |\n---------------------\n|  ${board[7]}  ||  ${board[8]}  ||  ${board[9]}  |"
 
 }
 
@@ -53,9 +55,10 @@ function switchPlayer() {
 
 }
 
-
 resettingBoard
-assignSymbol
 toss
+assignSymbol
 displayBoard
 switchPlayer
+displayBoard
+
