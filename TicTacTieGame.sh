ecc8387d22a0e8fdba46577363cd1560fa22a1f4
mounikachiluveri <43512972+mounikachiluveri@users.noreply.github.com>
2020-07-14 11:13:15 +0530

    Update TicTacTieGame.sh
    [Mounika] Refactor :Code refactored

--- TicTacTieGame.sh
+++ TicTacTieGame.sh
@@ -1,18 +1,8 @@
 #!/bin/bash -x
 echo -e "\n Welcome to Tic-Tac-Toe game.\n"
 echo -e "Player vs Computer\n"
-row=3
-column=3
-numberOfShell=$(($row*$column))
-function displayBoard(){
-          board=( . . . . . . . . . )
-          for (( columnValue=0,rowNumber=0;rowNumber<$row;columnValue=$(($columnValue+3)),rowNumber++ ))
-          do
-                   echo -e "\t${board[columnValue]} | ${board[columnValue+1]} | ${board[columnValue+2]}"
-                      if (($rowNumber!=$(($row-1))))
-                      then
-                           echo -e "\t---------"
-                      fi
-         done
-                      }
-displayBoard
+function displayBoard() {
+
+        echo -e "****** Tic-Tac-Toe ******* \n|  ${board[1]}  ||  ${board[2]}  ||  ${board[3]}  |\n---------------------\n| ${board[4]}   ||  ${board[5]}  ||  ${board[6]}  |\n---------------->
+
+}
